12312a0b6030b847acf0c96722ea0a816e2752ce
Leonardo Espinoza <58142872+LeoCode0@users.noreply.github.com>
2021-03-11 17:50:16 -0600

    styled statistics

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import customizable from "../images/icon-fully-customizable.svg";
 import recognition from "../images/icon-brand-recognition.svg";
 import records from "../images/icon-detailed-records.svg";
-import customizable from "../images/icon-fully-customizable.svg";
+import "../scss/statistics.scss";
 
 const Statistic = ({ icon, title, text }) => (
   <li className="statistic">
-    <img src={icon} alt={title} className="statistic__icon" />
-    <h2 className="statistic__title">{title}</h2>
-    <p className="statistic__description">{text}</p>
+    <div className="statistic__icon-container">
+      <img src={icon} alt={title} />
+    </div>
+    <h2 className="statistic__title title">{title}</h2>
+    <p className="statistic__description description ">{text}</p>
   </li>
 );
 
@@ -15,8 +18,8 @@ export const Statistics = () => {
   return (
     <div className="statistics">
       <div className="statistics__info">
-        <div className="statistics__title">Advanced Statistics</div>
-        <div className="statistics__description">
+        <div className="statistics__title title">Advanced Statistics</div>
+        <div className="statistics__description description">
           Track how your links are performing across the web with our advanced
           statistics dashboard.
         </div>
@@ -28,12 +31,14 @@ export const Statistics = () => {
           text="Boost your brand recognition with each click. Generic links don’t
                   mean a thing. Branded links help instil confidence in your content."
         />
+        <li className="separator"></li>
         <Statistic
           icon={records}
           title="Detailed Records"
           text="Gain insights into who is clicking your links. Knowing when and where
-                  people engage with your content helps inform better decisions."
+          people engage with your content helps inform better decisions."
         />
+        <li className="separator"></li>
         <Statistic
           icon={customizable}
           title="Fully Customizable"
